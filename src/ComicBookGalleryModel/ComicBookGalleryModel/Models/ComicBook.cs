dbae660c50d24520487759c69ba20a1262e8498a
James Churchill <james.churchill@teamtreehouse.com>
2016-11-14 19:27:46 -0800

    Added the project and the ComicBook entity class

---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedOn { get; set; }
+    }
+}
