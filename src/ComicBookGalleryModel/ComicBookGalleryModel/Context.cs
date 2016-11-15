2580904e997fa0d72815f40d0dbe8319ca1dd373
James Churchill <james.churchill@teamtreehouse.com>
2016-11-15 10:46:06 -0800

    Added the Context class

---
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -0,0 +1,15 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    public class Context : DbContext
+    {
+        public DbSet<ComicBook> ComicBooks { get; set; }
+    }
+}
