66a8bbc0f05feff593d883ed4f1bb1bc85bd48ec
James Churchill <james.churchill@teamtreehouse.com>
2016-11-16 15:52:11 -0800

    Added SeriesId foreign key property

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,13 @@ namespace ComicBookGalleryModel.Models
     public class ComicBook
     {
         public int Id { get; set; }
-        public Series Series { get; set; }
+        public int SeriesId { get; set; }
         public int IssueNumber { get; set; }
         public string Description { get; set; }
         public DateTime PublishedOn { get; set; }
         public decimal? AverageRating { get; set; }
+
+        public Series Series { get; set; }
 
         public string DisplayText
         {
