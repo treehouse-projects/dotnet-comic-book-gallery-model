06f932936d8f3ffc55f33465b2fbdba42d820908
James Churchill <james.churchill@teamtreehouse.com>
2016-11-17 15:25:22 -0800

    Added data annotation attributes to the Artist, Role, and Series entity classes

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Models/Artist.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Models/Artist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace ComicBookGalleryModel.Models
         }
 
         public int Id { get; set; }
+        [Required, StringLength(100)]
         public string Name { get; set; }
 
         public ICollection<ComicBookArtist> ComicBooks { get; set; }
