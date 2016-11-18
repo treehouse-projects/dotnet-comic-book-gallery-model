f4fe7b153e99a60989f03f21a315e20f5c808411
James Churchill <james.churchill@teamtreehouse.com>
2016-11-17 17:14:42 -0800

    Added DatabaseInitializer class

--- src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -13,7 +13,7 @@ namespace ComicBookGalleryModel
     {
         public Context()
         {
-            Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+            Database.SetInitializer(new DatabaseInitializer());
         }
 
         public DbSet<ComicBook> ComicBooks { get; set; }
--- src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
+++ src/ComicBookGalleryModel/ComicBookGalleryModel/Program.cs
@@ -14,69 +14,6 @@ namespace ComicBookGalleryModel
         {
             using (var context = new Context())
             {
-                var series1 = new Series()
-                {
-                    Title = "The Amazing Spider-Man"
-                };
-                var series2 = new Series()
-                {
-                    Title = "The Invincible Iron Man"
-                };
-
-                var artist1 = new Artist()
-                {
-                    Name = "Stan Lee"
-                };
-                var artist2 = new Artist()
-                {
-                    Name = "Steve Ditko"
-                };
-                var artist3 = new Artist()
-                {
-                    Name = "Jack Kirby"
-                };
-
-                var role1 = new Role()
-                {
-                    Name = "Script"
-                };
-                var role2 = new Role()
-                {
-                    Name = "Pencils"
-                };
-
-                var comicBook1 = new ComicBook()
-                {
-                    Series = series1,
-                    IssueNumber = 1,
-                    PublishedOn = DateTime.Today
-                };
-                comicBook1.AddArtist(artist1, role1);
-                comicBook1.AddArtist(artist2, role2);
-
-                var comicBook2 = new ComicBook()
-                {
-                    Series = series1,
-                    IssueNumber = 2,
-                    PublishedOn = DateTime.Today
-                };
-                comicBook2.AddArtist(artist1, role1);
-                comicBook2.AddArtist(artist2, role2);
-
-                var comicBook3 = new ComicBook()
-                {
-                    Series = series2,
-                    IssueNumber = 1,
-                    PublishedOn = DateTime.Today
-                };
-                comicBook3.AddArtist(artist1, role1);
-                comicBook3.AddArtist(artist3, role2);
-
-                context.ComicBooks.Add(comicBook1);
-                context.ComicBooks.Add(comicBook2);
-                context.ComicBooks.Add(comicBook3);
-                context.SaveChanges();
-
                 var comicBooks = context.ComicBooks
                     .Include(cb => cb.Series)
                     .Include(cb => cb.Artists.Select(a => a.Artist))
